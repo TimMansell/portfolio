0300427faf3a9288c876738ef26768bcf2daf81e
TimMansell <mansell.tim@gmail.com>
2015-05-03 16:28:08 +1000

    Tidy up mobile layout

--- app/assets/js/ng/directives/nav.js
+++ app/assets/js/ng/directives/nav.js
@@ -17,45 +17,16 @@
             elementFromTop = element.offset().top,
             showFromBottom = (attrs.showFromBottom) ? attrs.showFromBottom : 50;
 
-        // Set height so element keeps it's height when text dissapears.
-        element.css('height', element.height());
-
         // Calculate on scroll
         $window.on('scroll', function(){
 
           // We have scrolled to the element, now do your stuff!
           if($window.scrollTop() >= elementFromTop){
-            element.addClass('is-sticky');
+            element.addClass('navigation-menu-is-sticky');
           } else {
-            element.removeClass('is-sticky');
+            element.removeClass('navigation-menu-is-sticky');
           }
         });
-
-        // Stick Navigation.
-        /*element.stickyNavbar({
-          mobileWidth: appConfig.breakpoints.xs,
-          stickyModeClass: 'is-sticky'
-          //startAt: 500
-        });*/
-
-        /*element.find('ul').onePageNav({
-          scrollSpeed: 400 
-        });*/
-
-        /*$window.on('scroll', function(){
-          if($window.scrollTop() < $window.height()/2) {
-            element.find('li').removeClass('current');
-            //element.
-            //scope.
-          }
-        });
-
-        $window.on('resize', function(e){ 
-          console.log('e', e);
-          if(e.target.innerWidth > appConfig.breakpoints.xs){
-
-          }
-        });*/
       }
     }
 })();
