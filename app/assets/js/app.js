12010a4a8730663dde8f797efd9d8460ddd55b81
TimMansell <mansell.tim@gmail.com>
2017-03-26 18:58:32 +1100

    Implement react date component

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,9 +1,20 @@
 import angular from 'angular';
 import angularScroll from 'angular-scroll';
 
-import components from './components/app.components';
+import components from './components/ng/app.components';
 
-import webFonts from './components/webFonts/webFonts';
+import webFonts from './components/ng/webFonts/webFonts';
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Copyright} from './components/react/date/date';
+
+ReactDOM.render(
+  <Copyright />,
+  document.getElementById('date')
+);
 
 // Init app with dependencies.
 export default angular.module('TM', ['duScroll', components]);
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ var config = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['react', 'es2015']
         }
       },
       {
