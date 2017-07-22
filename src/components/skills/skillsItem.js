b6eee3bd7828ea54bd12275b4ef381f086b2941e
TimMansell <mansell.tim@gmail.com>
2017-07-22 11:47:33 +1000

    Refactors skills section: create skillsItem component

---
+++ src/components/skills/skillsItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classnames from 'classnames';
+
+export default class SkillsItem extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+    render() {
+		let iconClass = classnames('fa skills__icon', this.props.skill.icon);
+
+        return (
+			<div>
+				<i className={iconClass}></i>
+				<ul className="skills__list">
+					{this.props.skill.list.map((item, i) =>
+						<li key={i}>{item}</li>
+					)}
+				</ul>
+			</div>
+		);
+    }
+}
