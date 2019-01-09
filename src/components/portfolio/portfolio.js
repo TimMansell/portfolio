2660997d013b7fc6786ad24dfc51975b90998f8a
Tim Mansell <mansell.tim@gmail.com>
2019-01-09 15:12:10 +1100

    Feature/portfolio update (#3)
    * Update broken porfolio website links to web.archive.org
    
    * Add myBupa to portfolio
    
    * Re-arrange website order

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -6,22 +6,30 @@ import {ShuffleCharacters} from '../shuffleCharacters/shuffleCharacters';
 
 export class Portfolio extends React.Component {
     render() {
-		let items = [
+		const items = [
+			{
+				"title": "myBupa",
+				"src": require('./my-bupa.jpg'),
+				"thumb": require('./my-bupa-thb.jpg'),
+				"description": "Responsive website rebuild.  Built on top of .NET stack.",
+				"tech": "AngularJS, Bootstrap 3, SASS, ES6/Babel, and Webpack.",
+				"url": "https://my.bupa.com.au"
+			},
 			{
 				"title": "Captain's Choice",
 				"src": require('./captains-choice.jpg'),
 				"thumb": require('./captains-choice-thb.jpg'),
 				"description": "Responsive website rebuild. Built on top of .NET stack.",
 				"tech": "AngularJS, Bootstrap 3, SCSS/Compass, jQuery, Gulp, and Bower.",
-				"url": "http://www.captainschoice.com.au/"
+				"url": "https://web.archive.org/web/20160228234736/http://www.captainschoice.com.au/"
 			},
 			{
-				"title": "Tourism Victoria - Mobile site",
-				"src": require('./tourism-victoria-mobile-site.jpg'),
-				"thumb": require('./tourism-victoria-mobile-site-thb.jpg'),
-				"description": "Dedicated mobile site build. Built on top of .NET stack.",
-				"tech": "AngularJS, Bootstrap 2, LESS, and jQuery.",
-				"url": "http://m.visitvictoria.com/"
+				"title": "Tourism Victoria",
+				"src": require('./visit-victoria.jpg'),
+				"thumb": require('./visit-victoria-thb.jpg'),
+				"description": "Responsive website rebuild.  Built on top of .NET stack.",
+				"tech": "AngularJS, Bootstrap 3, SASS, Gulp, Bower, and jQuery.",
+				"url": "http://visitvictoria.com/"
 			},
 			{
 				"title": "UniSuper",
@@ -37,15 +45,7 @@ export class Portfolio extends React.Component {
 				"thumb": require('./point-and-claim-thb.jpg'),
 				"description": "Responsive website rebuild. Built on top of CodeIgnitor framework.",
 				"tech": "AngularJS, Bootstrap 3, SCSS/Compass, jQuery, and Gulp.",
-				"url": "http://pointandclaim.com/"
-			},
-			{
-				"title": "Tourism Victoria",
-				"src": require('./visit-victoria.jpg'),
-				"thumb": require('./visit-victoria-thb.jpg'),
-				"description": "Responsive website rebuild.  Built on top of .NET stack.",
-				"tech": "AngularJS, Bootstrap 3, SASS, Gulp, Bower, and jQuery.",
-				"url": "http://visitvictoria.com/"
+				"url": "https://web.archive.org/web/20160622073557/http://pointandclaim.com/"
 			},
 			{
 				"title": "Simply Great Meals",
