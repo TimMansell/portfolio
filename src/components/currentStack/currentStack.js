242f11bc2ba983a44999728d6f38c805fb9bb021
TimMansell <mansell.tim@gmail.com>
2019-01-13 19:49:17 +1100

    Duplicate skills component as base for CurrentStack component

---
+++ src/components/currentStack/currentStack.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import InViewport from '../inViewport/inViewport';
+import {ShuffleCharacters} from '../shuffleCharacters/shuffleCharacters';
+
+import SkillsItem from './skillsItem';
+
+import skills from './skills.json';
+import learning from './learning.json';
+
+export class Skills extends React.Component {
+    render() {
+        return <section id="skills" className="layout-section bg--primary text--center">
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<div className="heading text--secondary">
+							<InViewport>
+								<ShuffleCharacters>
+									<h2 className="heading__title text--center">Skills</h2>
+								</ShuffleCharacters>
+							</InViewport>
+							<p className="heading__description">I spend most of my time with these technologies / tools / methodologies</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="row skills">
+					{skills.map((skill, i) =>
+						<div className="col-12 col-sm-6 col-md-4 col-lg-2 text--center text--secondary divider" key={i}>
+							<SkillsItem skill={skill} />
+						</div>
+					)}
+				</div>
+
+				<p className="text-description text--secondary">I am currently playing around with</p>
+				<div className="row skills">
+					{learning.map((skill, i) =>
+						<div className="col-12 text--center text--secondary" key={i}>
+							<SkillsItem skill={skill} />
+						</div>
+					)}
+				</div>
+			</div>
+		</section>;
+    }
+}
