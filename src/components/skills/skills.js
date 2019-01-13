f8742908960734556629a2d5a051a7b5369b0fdb
TimMansell <mansell.tim@gmail.com>
2019-01-13 19:29:21 +1100

    Update skills

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -5,84 +5,11 @@ import {ShuffleCharacters} from '../shuffleCharacters/shuffleCharacters';
 
 import SkillsItem from './skillsItem';
 
+import skills from './skills.json';
+import learning from './learning.json';
+
 export class Skills extends React.Component {
     render() {
-		const skills = [
-			{
-				list: [
-					'Semantic HTML5',
-					'Bootstrap 3/4',
-					'SEO',
-					'Accessibility',
-					'Web Components'
-				],
-				icon: 'fa-html5'
-			},
-			{
-				list: [
-					'CSS3',
-					'SCSS / LESS',
-					'BEM',
-					'OOCSS / SMACSS',
-					'PostCSS',
-					'Atomic Design'
-				],
-				icon: 'fa-css3'
-			},
-			{
-				list: [
-					'Responsive',
-					'Mobile First',
-					'Performance',
-					'Optimisation'
-				],
-				icon: 'fa-mobile'
-			},
-			{
-				list: [
-					'ES5 / ES6',
-					'Babel',
-					'AngularJS',
-					'React / Redux',
-					'Backbone.js',
-					'JSON',
-					'jQuery'
-				],
-				icon: 'fa-code'
-			},
-			{
-				list: [
-					'Node.js',
-					'NPM / Yarn',
-					'Gulp / Grunt',
-					'Git / GitFlow / SVN',
-					'Webpack 3'
-				],
-				icon: 'fa-terminal'
-			},
-			{
-				list: [
-					'Agile',
-					'Scrum',
-					'Kanban',
-					'JIRA / VSTS',
-					'Table Tennis'
-				],
-				icon: 'fa-paper-plane'
-			}
-		];
-
-		const learning = [
-			{
-				list: [
-					'WebVR / ReactVR',
-					'Functional Programming',
-					'CSS Grid'
-				],
-				icon: 'fa-gamepad'
-			}
-		];
-
         return <section id="skills" className="layout-section bg--primary text--center">
 			<div className="container">
 				<div className="row">
