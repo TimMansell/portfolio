3b1a36e945e2c2f8516e23ee6785d9c6711ac51d
TimMansell <mansell.tim@gmail.com>
2019-04-26 15:41:06 +1000

    Migrate container classes to <SectionWrap>
    Remove global breakpoints include

--- src/layout/SectionWrap/SectionWrap.js
+++ src/layout/SectionWrap/SectionWrap.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import './SectionWrap.scss';
+
 const SectionWrap = ({container, background, id="", children}) => {
 	const sectionClasses = classnames('layout-section', {
 		'bg--primary': background === 'primary',
