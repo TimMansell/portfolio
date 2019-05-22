0ff7e33c8988d2cc72286dd55537ef37d2b549c2
Tim Mansell <mansell.tim@gmail.com>
2019-05-22 14:08:58 +1000

    Feat/tag line animation (#18) (#19)
    * Migrate intro section into seperate components
    
    install animejs
    
    * First pass at animating intro
    
    * remove animjs
    
    Migrate Name and Location to seperate components
    Update Tagline css names
    Move hero class into Hero component
    
    * Tweak animations

---
+++ src/components/Hi/Hi.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Hi.scss';
+
+export const Hi = () => {
+	return <p className="hi">Hi!</p>;
+}
+
+export default Hi;
---
+++ src/components/Intro/IntroLocation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Intro.scss';
+
+export const IntroLocation = () => {
+	return <h4 className="info__text info__text--sub-title">Melbourne, Australia</h4>;
+}
+
+export default IntroLocation;
