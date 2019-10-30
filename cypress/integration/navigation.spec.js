ecf79a4e049bf5e16d027b9eb39f3bceb3092655
TimMansell <mansell.tim@gmail.com>
2019-10-30 13:57:57 +1100

    Add desktop e2e tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,71 @@
+const url = '/';
+
+describe('Navigation', () => {
+  beforeEach(() => {
+    cy.visit(url);
+  });
+
+  it('clicking on profile item should scroll page to profile section', () => {
+    cy.get('[data-e2e="navigation-profile"]')
+      .click();
+
+    cy.get('#profile')
+      .should('be.visible');
+  });
+
+  it('clicking on skills item should scroll page to skills section', () => {
+    cy.get('[data-e2e="navigation-skills"]')
+      .click();
+
+    cy.get('#skills')
+      .should('be.visible');
+  });
+
+  it('clicking on retired skills item should scroll page to retired skills section', () => {
+    cy.get('[data-e2e="navigation-retired-skills"]')
+      .click();
+
+    cy.get('#retired-skills')
+      .should('be.visible');
+  });
+
+  it('clicking on stack item should scroll page to stack section', () => {
+    cy.get('[data-e2e="navigation-stack"]')
+      .click();
+
+    cy.get('#stack')
+      .should('be.visible');
+  });
+
+  it('clicking on portfolio item should scroll page to portfolio section', () => {
+    cy.get('[data-e2e="navigation-portfolio"]')
+      .click();
+
+    cy.get('#portfolio')
+      .should('be.visible');
+  });
+
+  it('clicking on presentations item should scroll page to presentations section', () => {
+    cy.get('[data-e2e="navigation-presentations"]')
+      .click();
+
+    cy.get('#presentations')
+      .should('be.visible');
+  });
+
+  it('clicking on testimonials item should scroll page to testimonials section', () => {
+    cy.get('[data-e2e="navigation-testimonials"]')
+      .click();
+
+    cy.get('#testimonials')
+      .should('be.visible');
+  });
+
+  it('clicking on contact item should scroll page to contact section', () => {
+    cy.get('[data-e2e="navigation-contact"]')
+      .click();
+
+    cy.get('#contact')
+      .should('be.visible');
+  });
+});
