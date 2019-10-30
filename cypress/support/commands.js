17c7d3de1b29790cdb59476d786b0229692d8fd7
TimMansell <mansell.tim@gmail.com>
2019-10-30 14:24:02 +1100

    Add snapshot testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('checkDesktopSnapshot', (el) => {
+  cy.viewport('macbook-11');
+  cy.matchImageSnapshot('desktop');
+});
+
+Cypress.Commands.add('checkMobileSnapshot', (el) => {
+  cy.viewport('iphone-5');
+  cy.matchImageSnapshot('mobile');
+});
+
+addMatchImageSnapshotCommand({
+  failureThreshold: 5, // threshold for entire image
+  failureThresholdType: 'percent' // percent of image or number of pixels
+});
