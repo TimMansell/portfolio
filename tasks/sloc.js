e2351e553cfe7da840afaad2cd05cceb3d084933
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-06-25 19:50:20 +1000

    install fs-extra and glob.  create js file to calculate lines of code

---
+++ tasks/sloc.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra');
+const glob = require('glob');
+const sloc = require('sloc');
+
+const file = './src/components/Stats/json/stats.json';
+
+glob('./src/**/*.?(js|scss)', {}, (er, files) => {
+  const countLoc = (currentValue, file) => {
+    const code = fs.readFileSync(file, 'utf8');
+    const { source } = sloc(code, 'js');
+
+    return source + currentValue;
+  };
+
+  const loc = files.reduce(countLoc, 0);
+
+  console.log('loc', loc);
+  fs.outputJsonSync(file, { loc: loc });
+});
