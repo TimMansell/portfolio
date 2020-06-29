f83de082d7a74803a1644e2c9bd6bd45a3055a45
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-06-29 14:59:53 +1000

    update title to stats. update to pull stats from stats.json

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import differenceInDays from 'date-fns/differenceInDays';
 
 import Header from 'components/Header';
 import InViewport from 'components/InViewport';
@@ -17,19 +16,19 @@ import {
 } from 'components/Icon';
 
 export const Stats = () => {
-  const COFFEES_PER_DAY = 3;
-  const coffeesSinceFirstCommit =
-    differenceInDays(new Date(), new Date(2012, 6, 2, 0, 0)) * COFFEES_PER_DAY;
-
   return (
     <>
-      <Header title="Effort" text="This website was built using" tertiary />
+      <Header title="Stats" text="This website was built using" tertiary />
       <div className={styles.lighterNote}>
         <div className={styles.item}>
           <IconCode padded />
           <InViewport>
             <p className={styles.amount}>
-              <Counter begin={stats.loc - 1000} end={stats.loc} time={2000} />
+              <Counter
+                begin={Math.round(stats.loc * 0.9)}
+                end={stats.loc}
+                time={2000}
+              />
             </p>
           </InViewport>
           <p className={styles.text}>lines of code</p>
@@ -38,7 +37,11 @@ export const Stats = () => {
           <IconCodeBranch padded />
           <InViewport>
             <p className={styles.amount}>
-              <Counter begin={300} end={1200} time={2000} />
+              <Counter
+                begin={Math.round(stats.commits * 0.5)}
+                end={stats.commits}
+                time={2000}
+              />
             </p>
           </InViewport>
           <p className={styles.text}>git commits</p>
@@ -47,7 +50,7 @@ export const Stats = () => {
           <IconCodeLaptop padded />
           <InViewport>
             <p className={styles.amount}>
-              <Counter begin={300} end={1200} time={2000} />
+              <Counter begin={10} end={stats.pullRequests} time={2000} />
             </p>
           </InViewport>
           <p className={styles.text}>pull requests</p>
@@ -56,7 +59,7 @@ export const Stats = () => {
           <IconMugHot padded />
           <InViewport>
             <p className={styles.amount}>
-              <Counter begin={200} end={coffeesSinceFirstCommit} time={2000} />
+              <Counter begin={0} end={stats.coffees} time={2000} />
             </p>
           </InViewport>
           <p className={styles.text}>coffees consumed</p>
