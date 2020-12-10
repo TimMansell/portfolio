af7807981234f74448b51ca8d57cf22c6a43eed1
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-12-10 15:33:03 +1100

    update testimonialsJson > testimonials and settings > sliderSettings

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Slider from 'react-slick';
 
 import TestimonialItem from './TestimonialItem';
-
-import testimonialsJson from './json/testimonials.json';
-import styles from './Testimonials.module.scss';
-
 import { IconQuoteLeft, IconQuoteRight } from '../Icon';
 
+import testimonials from './json/testimonials.json';
+import styles from './Testimonials.module.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const settings = {
+const sliderSettings = {
   adaptiveHeight: true,
   arrows: false,
   autoplay: true,
@@ -21,10 +19,10 @@ const settings = {
 
 export const Testimonials = () => {
   return (
-    <div id="test" className={styles.testimonials}>
+    <div className={styles.testimonials}>
       <IconQuoteLeft className={styles.quote} />
-      <Slider {...settings} className={styles.slider}>
-        {testimonialsJson.map((testimonial, index) => (
+      <Slider {...sliderSettings} className={styles.slider}>
+        {testimonials.map((testimonial, index) => (
           <div key={index}>
             <TestimonialItem {...testimonial} />
           </div>
