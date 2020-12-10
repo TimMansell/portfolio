f69ef2a185983678cc6bf1d3497d20df33264084
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-12-10 15:32:16 +1100

    remove snapshot test from Main.test.js

--- src/layout/Main/__tests__/Main.test.js
+++ src/layout/Main/__tests__/Main.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
 
 import Main from '../Main';
 
@@ -8,11 +7,5 @@ describe('Main', () => {
   it('should render my component', () => {
     // eslint-disable-next-line
     const wrapper = shallow(<Main />);
-  });
-
-  it('should match snapshot', () => {
-    const snapshot = renderer.create(<Main />).toJSON();
-
-    expect(snapshot).toMatchSnapshot();
   });
 });
