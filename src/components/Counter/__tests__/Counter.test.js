7603b5e4414adbd93530cd797b852056fe6c9961
TimMansell <1926461+TimMansell@users.noreply.github.com>
2020-12-14 14:46:52 +1100

    begin number unit test

--- src/components/Counter/__tests__/Counter.test.js
+++ src/components/Counter/__tests__/Counter.test.js
@@ -12,12 +12,18 @@ const props = {
 describe('Counter', () => {
   it('should render my component', () => {
     // eslint-disable-next-line
-    const wrapper = shallow(<Counter {...props}/>);
+    const wrapper = shallow(<Counter {...props} />);
   });
 
   it('should match snapshot', () => {
     const snapshot = renderer.create(<Counter {...props} />).toJSON();
 
     expect(snapshot).toMatchSnapshot();
+  });
+
+  it('should start at correct number', () => {
+    const wrapper = shallow(<Counter {...props} />);
+
+    expect(wrapper.text()).toEqual(`${props.begin}`);
   });
 });
