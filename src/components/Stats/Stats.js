d95198ad762ba71a0abb9d4dd9b77f751d2cc98f
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-01-13 15:39:44 +1100

    migrate components to use useInViewport custom hook

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import Header from 'components/Header';
-import InViewport from 'components/InViewport';
 import Counter from 'components/Counter';
+
+import useInViewport from 'hooks/useInViewport';
 
 import stats from './json/stats.json';
 
@@ -16,52 +17,55 @@ import {
 } from 'components/Icon';
 
 export const Stats = () => {
+  const element = useRef(null);
+  const isInViewport = useInViewport(element);
+
   return (
     <>
       <Header title="Stats" text="This website was built using" tertiary />
-      <div className={styles.lighterNote}>
+      <div className={styles.lighterNote} ref={element}>
         <div className={styles.item}>
           <IconCode padded />
-          <InViewport>
-            <p className={styles.amount}>
+          <p className={styles.amount}>
+            {isInViewport && (
               <Counter
                 begin={Math.round(stats.loc * 0.9)}
                 end={stats.loc}
                 time={2000}
               />
-            </p>
-          </InViewport>
+            )}
+          </p>
           <p className={styles.text}>lines of code</p>
         </div>
         <div className={styles.item}>
           <IconCodeBranch padded />
-          <InViewport>
-            <p className={styles.amount}>
+          <p className={styles.amount}>
+            {isInViewport && (
               <Counter
                 begin={Math.round(stats.commits * 0.5)}
                 end={stats.commits}
                 time={2000}
               />
-            </p>
-          </InViewport>
+            )}
+          </p>
           <p className={styles.text}>git commits</p>
         </div>
         <div className={styles.item}>
           <IconCodeLaptop padded />
-          <InViewport>
-            <p className={styles.amount}>
+          <p className={styles.amount}>
+            {isInViewport && (
               <Counter begin={10} end={stats.pullRequests} time={2000} />
-            </p>
-          </InViewport>
+            )}
+          </p>
           <p className={styles.text}>pull requests</p>
         </div>
         <div className={styles.item}>
           <IconMugHot padded />
-          <InViewport>
-            <p className={styles.amount}>
+          <p className={styles.amount}>
+            {isInViewport && (
               <Counter begin={0} end={stats.coffees} time={2000} />
-            </p>
-          </InViewport>
+            )}
+          </p>
           <p className={styles.text}>coffees</p>
         </div>
       </div>
