8e4bf8b405fadff7ae271ad77730d68e03433cdd
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-02-15 21:01:55 +1100

    refactor menu items to navigation.json

--- src/components/Navigation/NavigationMenu.js
+++ src/components/Navigation/NavigationMenu.js
@@ -4,118 +4,48 @@ import classnames from 'classnames';
 import { Link } from 'react-scroll';
 
 import styles from './Navigation.module.scss';
+import navigationJson from './json/navigation.json';
+
+export const combineWords = (words) => words.replaceAll(' ', '-');
+
+export const splitWords = (words) => words.split(' ');
+
+export const upperCaseWord = ([initial, ...rest]) =>
+  [initial.toUpperCase(), ...rest].join('');
+
+export const navigationName = (name) => {
+  const words = splitWords(name);
+
+  return words.map((word) => upperCaseWord(word)).join(' ');
+};
 
 export const NavigationMenu = () => {
   const [isMobileMenu, setIsMobileMenu] = useContext(MenuContext);
-
-  const closeMenu = () => {
-    setIsMobileMenu(false);
-  };
 
   const menuClasses = classnames(styles.menu, {
     [styles.menuActive]: isMobileMenu,
   });
 
+  const closeMenu = () => setIsMobileMenu(false);
+
   return (
     <ul className={menuClasses}>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-profile"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="profile"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Profile
-        </Link>
-      </li>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-skills"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="skills"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Skills
-        </Link>
-      </li>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-retired-skills"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="retired-skills"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Retired Skills
-        </Link>
-      </li>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-stack"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="stack"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Stack
-        </Link>
-      </li>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-portfolio"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="portfolio"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-testimonials"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="testimonials"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Testimonials
-        </Link>
-      </li>
-      <li className={styles.item}>
-        <Link
-          data-e2e="navigation-contact"
-          className={styles.link}
-          activeClass={styles.linkActive}
-          to="contact"
-          smooth={true}
-          spy={true}
-          duration={500}
-          onClick={() => closeMenu()}
-        >
-          Contact
-        </Link>
-      </li>
+      {navigationJson.map((navigation, index) => (
+        <li className={styles.item} key={index}>
+          <Link
+            data-e2e={`navigation-${combineWords(navigation)}`}
+            className={styles.link}
+            activeClass={styles.linkActive}
+            to={combineWords(navigation)}
+            smooth={true}
+            spy={true}
+            duration={500}
+            onClick={() => closeMenu()}
+          >
+            {navigationName(navigation)}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
