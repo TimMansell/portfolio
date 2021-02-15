67aa28d2255e476d90e552dd86b76b692caca3c9
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-02-15 20:17:19 +1100

    update CurrentStack tests

--- src/components/CurrentStack/__tests__/CurrentStack.test.js
+++ src/components/CurrentStack/__tests__/CurrentStack.test.js
@@ -4,25 +4,6 @@ import renderer from 'react-test-renderer';
 
 import CurrentStack from '../CurrentStack';
 
-// jest.mock(
-//   '../json/stack.json',
-//   () => [
-//     {
-//       name: 'Item 1',
-//       url: 'url',
-//       img: 'react.svg',
-//       width: '65',
-//     },
-//     {
-//       name: 'Item 2',
-//       url: 'url',
-//       img: 'nodejs.svg',
-//       width: '90',
-//     },
-//   ],
-//   { virtual: true }
-// );
-
 describe('CurrentStack', () => {
   it('should render my component', () => {
     // eslint-disable-next-line
@@ -34,11 +15,4 @@ describe('CurrentStack', () => {
 
     expect(snapshot).toMatchSnapshot();
   });
-
-  // it('should render correct amount of items', () => {
-  //   const wrapper = shallow(<CurrentStack />);
-  //   const list = wrapper.find('[data-test="stack-items"]');
-
-  //   expect(list.children()).toHaveLength(2);
-  // });
 });
