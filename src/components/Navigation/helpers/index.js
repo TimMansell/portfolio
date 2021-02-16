59300e9cfe52ae245c40e11caef7fabad97f90e1
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-02-16 18:04:04 +1100

    refactor combineWords to use regex

--- src/components/Navigation/helpers/index.js
+++ src/components/Navigation/helpers/index.js
@@ -1,12 +1,8 @@
-export const combineWords = (words) => words.replace(' ', '-');
+import {
+  combineWords,
+  splitWords,
+  upperCaseWord,
+  upperCaseWords,
+} from './helpers';
 
-export const splitWords = (words) => words.split(' ');
-
-export const upperCaseWord = ([initial, ...rest]) =>
-  [initial.toUpperCase(), ...rest].join('');
-
-export const upperCaseWords = (name) => {
-  const words = splitWords(name);
-
-  return words.map((word) => upperCaseWord(word)).join(' ');
-};
+export { combineWords, splitWords, upperCaseWord, upperCaseWords };
