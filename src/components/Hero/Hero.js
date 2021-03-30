fcbe538954a99f1d75e0b53f9660a8690ce93ebf
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-03-30 16:34:02 +1100

    Hide hero image on mobile, add background overlay to image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
+import Picture from '../Picture';
 import useScrollBlur from 'hooks/useScrollBlur';
+import useImageFormats from 'hooks/useImageFormats';
 
 import styles from './Hero.module.scss';
 
+const img = {
+  name: 'maria-vojtovicova-SPvJPDXEmqA-unsplash',
+  formats: ['jpg'],
+  fallback: 'jpg',
+};
+
 export const Hero = () => {
-  const scrollStyles = useScrollBlur(1, 10);
+  const scrollStyles = useScrollBlur(0, 10);
+  const { srcs, defaultImg } = useImageFormats(img);
+
+  const pictureSources = srcs.map(({ format, src }) => ({
+    format,
+    src: require(`./img/${src}`),
+  }));
 
   return (
     <div className={styles.hero}>
-      <div
-        className={styles.img}
-        style={scrollStyles}
-        data-e2e="hero-img"
-      ></div>
+      <div className={styles.img} style={scrollStyles} data-e2e="hero-img">
+        <Picture
+          srcs={pictureSources}
+          name={img.name}
+          defaultImg={require(`./img/${defaultImg}`)}
+        />
+      </div>
     </div>
   );
 };
