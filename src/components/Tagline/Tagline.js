80e23349315ae7e6c30e63d552654035d2bdc090
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-03-30 16:34:17 +1100

    Temp hide other taglines

--- src/components/Tagline/Tagline.js
+++ src/components/Tagline/Tagline.js
@@ -6,9 +6,9 @@ export const Tagline = () => {
   return (
     <div className={styles.tagline}>
       <div className={styles.item}>Front-end Engineer</div>
-      <div className={styles.item}>Javascript Engineer</div>
+      {/* <div className={styles.item}>Javascript Engineer</div>
       <div className={styles.item}>React Engineer</div>
-      <div className={styles.item}>VueJS Engineer</div>
+      <div className={styles.item}>VueJS Engineer</div> */}
     </div>
   );
 };
