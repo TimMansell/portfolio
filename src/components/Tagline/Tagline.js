2003ba74031cf52ad689e1a674e9c9f7e0f81453
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-04-03 13:48:45 +1100

    create .hero, .shadow, .shadowLg classes

--- src/components/Tagline/Tagline.js
+++ src/components/Tagline/Tagline.js
@@ -5,7 +5,7 @@ import styles from './Tagline.module.scss';
 export const Tagline = () => {
   return (
     <div className={styles.tagline}>
-      <div className={styles.item}>Front-end Engineer</div>
+      <div>Front-end Engineer</div>
       {/* <div className={styles.item}>Javascript Engineer</div>
       <div className={styles.item}>React Engineer</div>
       <div className={styles.item}>VueJS Engineer</div> */}
