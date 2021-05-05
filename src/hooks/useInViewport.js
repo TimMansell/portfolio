9b1ee73bf334104ee780a4b638c3556ed1294faa
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-05-05 21:35:14 +1000

    refactor useInViewport hook to use IntersectionObserver

--- src/hooks/useInViewport.js
+++ src/hooks/useInViewport.js
@@ -1,14 +1,24 @@
 import { useState, useEffect } from 'react';
-import inViewport from 'in-viewport';
 
 export const useInViewport = (element, offset = 0) => {
   const [isInViewport, setIsInViewport] = useState(false);
 
   useEffect(() => {
-    const isVisible = () => setIsInViewport(true);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.intersectionRatio > 0) {
+          setIsInViewport(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      {
+        rootMargin: `0px 0px ${offset}px 0px`,
+        threshold: 1,
+      }
+    );
 
-    inViewport(element.current, { offset }, isVisible);
-  });
+    observer.observe(element.current);
+  }, [element, offset]);
 
   return isInViewport;
 };
