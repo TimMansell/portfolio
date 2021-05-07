bc581497f48224592563303956519ae460a6a3b7
TimMansell <1926461+TimMansell@users.noreply.github.com>
2021-05-07 13:43:53 +1000

    Add height to current stack images

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Image = ({ src, alt, width }) => {
+export const Image = ({ src, alt, width, height }) => {
   const img = require(`../${src}`);
 
-  return <img src={img} alt={alt} width={width} title={alt} loading="lazy" />;
+  return (
+    <img
+      src={img}
+      alt={alt}
+      width={width}
+      height={height}
+      title={alt}
+      loading="lazy"
+    />
+  );
 };
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
-  width: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
 };
 
 export default Image;
