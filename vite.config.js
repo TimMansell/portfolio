fae66e6746e22fa26fa627ba516934225b876f05
TimMansell <1926461+TimMansell@users.noreply.github.com>
2023-05-20 13:31:25 +1000

    create import aliases

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: { modules: { localsConvention: 'camelCase' } },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks/')}`,
+      layout: `${path.resolve(__dirname, './src/layout/')}`,
+      scss: `${path.resolve(__dirname, './src/scss/')}`,
+      utilities: `${path.resolve(__dirname, './src/scss/utilities/')}`,
+      public: `${path.resolve(__dirname, './public/')}`,
+    },
+  },
 });
