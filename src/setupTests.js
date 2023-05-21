58f9572acaced7ffed0d34b66183a629bcfc2636
TimMansell <1926461+TimMansell@users.noreply.github.com>
2023-05-21 15:33:31 +1000

    stub global functions for vitest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,14 +1,27 @@
+import { vi } from 'vitest';
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
 
-window.matchMedia =
-  window.matchMedia ||
-  function () {
-    return {
-      matches: false,
-      addListener: function () {},
-      removeListener: function () {},
-    };
-  };
+const matchMediaMock = vi.fn(() => ({
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+  matches: false,
+}));
+
+const IntersectionObserverMock = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  takeRecords: vi.fn(),
+  unobserve: vi.fn(),
+}));
+
+const RequestAnimationMock = vi.fn(() => ({
+  requestAnimationFrame: vi.fn(),
+  cancelAnimationFrame: vi.fn(),
+}));
+
+vi.stubGlobal('matchMedia', matchMediaMock);
+vi.stubGlobal('IntersectionObserver', IntersectionObserverMock);
+vi.stubGlobal('RequestAnimation', RequestAnimationMock);
