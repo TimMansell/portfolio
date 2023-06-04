62d1c9f53ac8f94a0c87857d83e37640082d5604
TimMansell <1926461+TimMansell@users.noreply.github.com>
2023-06-04 23:15:36 +1000

    remove enzyme from jest setup file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,3 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
 window.matchMedia =
   window.matchMedia ||
   function () {
