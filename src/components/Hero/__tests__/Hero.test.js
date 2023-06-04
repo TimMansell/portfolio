6c9337a660af2c4c1424bfc947105548e840953e
TimMansell <1926461+TimMansell@users.noreply.github.com>
2023-06-04 23:20:27 +1000

    remove should change images unit test

--- src/components/Hero/__tests__/Hero.test.js
+++ src/components/Hero/__tests__/Hero.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Hero from '../Hero';
-import images from '../json/images.json';
 
 describe('Hero', () => {
   beforeAll(() => {
@@ -22,17 +21,5 @@ describe('Hero', () => {
     const { asFragment } = render(<Hero />);
 
     expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('should change images', () => {
-    const { getAllByTestId } = render(<Hero />);
-
-    const element = getAllByTestId('picture')[0];
-
-    expect(element).toHaveAttribute('title', images[0].title);
-
-    act(() => jest.advanceTimersByTime(9000));
-
-    expect(element).toHaveAttribute('title', images[1].title);
   });
 });
