fdc20419e733767a89f01f2f8ce30415551a81ad
TimMansell <1926461+TimMansell@users.noreply.github.com>
2023-06-04 23:31:52 +1000

    remove useFlexGap hook as browser support has improved

--- src/components/CurrentStack/CurrentStackItems.js
+++ src/components/CurrentStack/CurrentStackItems.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import classnames from 'classnames';
 
 import CurrentStackItem from './CurrentStackItem';
 
@@ -7,17 +6,9 @@ import styles from './CurrentStack.module.scss';
 
 import stackItems from './json/stack.json';
 
-import { useIsFlexGapSupported } from 'hooks/useFlexGap';
-
 export const CurrentStackItems = () => {
-  const hasFlexGap = useIsFlexGapSupported();
-
-  const stackClasses = classnames(styles.stack, {
-    [styles.flexGap]: hasFlexGap,
-  });
-
   return (
-    <div className={stackClasses}>
+    <div className={styles.stack}>
       {stackItems.map((stack, i) => (
         <CurrentStackItem {...stack} key={i} />
       ))}
